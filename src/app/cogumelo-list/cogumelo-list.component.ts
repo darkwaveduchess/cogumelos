72447f684f8c7002518f8e415f5395e995195a26
Leonardo <l.guedes@ges.inatel.br>
2023-03-08 13:40:06 -0300

    add component

---
+++ src/app/cogumelo-list/cogumelo-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cogumelo-list',
+  templateUrl: './cogumelo-list.component.html',
+  styleUrls: ['./cogumelo-list.component.scss']
+})
+export class CogumeloListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
